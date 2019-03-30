0d94656bafe63ef9fbb0a911d6bbff51ae295a71
mdbiz <martindudebiz@gmail.com>
2019-03-30 13:30:53 +0100

    Removed evernote testing

--- test/test_option_parsing.py
+++ test/test_option_parsing.py
@@ -42,29 +42,6 @@ class TestOptions:
         assert(self.p.enable_filing)
         assert(self.p.config)
 
-    def test_standalone_filing_evernote(self):
-        # Check when evernote is enabled
-        opts = ["blah.pdf"]
-        opts.append('-e')
-        # Assert that it checks that the config file is present
-        with pytest.raises(SystemExit):
-            self.p.get_options(opts)
-
-        opts.append('--config=test_option_config.yaml')
-        self.p.get_options(opts)
-        # Enabling -e should turn on filing too
-        assert(self.p.enable_filing)
-        assert(self.p.enable_evernote)
-        assert(self.p.config)
-        assert(not self.p.watch)
-
-        opts.append('-f')
-        self.p.get_options(opts)
-        assert(self.p.enable_filing)
-        assert(self.p.enable_evernote)
-        assert(self.p.config)
-        assert(not self.p.watch)
-
     def test_standalone_watch_conflict(self):
         # When pdf file is specified, we don't want to allow watch option
         opts = ["blah.pdf", '-w']
@@ -86,20 +63,3 @@ class TestOptions:
         assert(self.p.watch)
         assert(self.p.config)
         assert(not self.p.enable_filing)
-        assert(not self.p.enable_evernote)
-
-    def test_watch_filing_evernote(self):
-        opts = ['-w temp', '-e', '--config=test_option_config.yaml']
-        self.p.get_options(opts)
-        assert(self.p.watch)
-        assert(self.p.config)
-        assert(self.p.enable_filing)
-        assert(self.p.enable_evernote)
-
-        opts = ['-w temp', '-f', '-e',  '--config=test_option_config.yaml']
-        self.p.get_options(opts)
-        assert(self.p.watch)
-        assert(self.p.config)
-        assert(self.p.enable_filing)
-        assert(self.p.enable_evernote)
-
