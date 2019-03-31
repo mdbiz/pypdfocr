dec4cf0f9c597fe8896c5d553160ffca1271c1d1
mdbiz <martindudebiz@gmail.com>
2019-03-31 15:48:55 +0200

    PEP8

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,13 @@
 from fabric.api import *
 import os
- 
-  
+
+
 def build_windows_dist():
     if os.name == 'nt':
         # Call the pyinstaller
-        local("python ../pyinstaller/pyinstaller.py pypdfocr_windows.spec --onefile")
+        local(
+            "python ../pyinstaller/pyinstaller.py pypdfocr_windows.spec --onefile"
+        )
 
 
 def run_tests():
@@ -13,11 +15,13 @@ def run_tests():
     with lcd(test_dir):
         # Regenerate the test script
         local("py.test --genscript=runtests.py")
-        t = local("py.test --cov-config .coveragerc --cov=pypdfocr --cov-report=term --cov-report=html", capture=False)
+        t = local(
+            "py.test --cov-config .coveragerc --cov=pypdfocr --cov-report=term --cov-report=html",
+            capture=False)
         t = local("coveralls")
 
-        #with open("test/COVERAGE.rst", "w") as f:
-            #f.write(t)
+        # with open("test/COVERAGE.rst", "w") as f:
+        # f.write(t)
 
 
 def push_docs():
@@ -26,14 +30,16 @@ def push_docs():
     """
     githubpages = "/Users/virantha/dev/githubdocs/pypdfocr"
     # Convert markdown readme to rst
-    #local("pandoc README.md -f markdown -t rst -o README.rst")
+    # local("pandoc README.md -f markdown -t rst -o README.rst")
     with lcd(githubpages):
         local("git checkout gh-pages")
         local("git pull origin gh-pages")
     local("head CHANGES.rst > CHANGES_RECENT.rst")
     local("tail -n 1 CHANGES.rst >> CHANGES_RECENT.rst")
     with lcd("docs"):
-        print("Running sphinx in docs/ and building to ~/dev/githubpages/pypdfocr")
+        print(
+            "Running sphinx in docs/ and building to ~/dev/githubpages/pypdfocr"
+        )
         local("make clean")
         local("make html")
         local("cp -R ../test/htmlcov %s/html/testing" % githubpages)
@@ -41,4 +47,3 @@ def push_docs():
         local("git add .")
         local('git commit -am "doc update"')
         local('git push origin gh-pages')
-
