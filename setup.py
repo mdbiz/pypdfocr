7fd6deea1824a046214a4ecda0822e16b11db217
mdbiz <martindudebiz@gmail.com>
2019-03-31 15:51:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,24 @@ from pypdfocr.version import __version__
 from setuptools import Command
 import os
 
+
 class PyTest(Command):
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        import sys,subprocess
+        import sys, subprocess
         cwd = os.getcwd()
         os.chdir('test')
         errno = subprocess.call([sys.executable, 'runtests.py'])
         os.chdir(cwd)
         raise SystemExit(errno)
+
 
 def read(*filenames, **kwargs):
     encoding = kwargs.get('encoding', 'utf-8')
@@ -30,6 +35,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
+
 packages = find_packages(exclude="tests")
 
 long_description = read('README.rst', 'CHANGES.rst', 'TODO.rst')
@@ -37,27 +43,26 @@ long_description = read('README.rst', 'CHANGES.rst', 'TODO.rst')
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-setup (
-    name = "pypdfocr",
-    version = __version__,
-    description="Converts a scanned PDF into an OCR'ed pdf using Tesseract-OCR and Ghostscript",
-    license = "ASL 2.0",
-    long_description = long_description,
+setup(
+    name="pypdfocr",
+    version=__version__,
+    description=
+    "Converts a scanned PDF into an OCR'ed pdf using Tesseract-OCR and Ghostscript",
+    license="ASL 2.0",
+    long_description=long_description,
     author="Virantha N. Ekanayake",
-    author_email="virantha@gmail.com", # Removed.
-    package_data = {'': ['*.xml']},
-    zip_safe = True,
-    include_package_data = True,
-    packages = packages,
-    install_requires = required,
-    entry_points = {
-            'console_scripts': [
-                    'pypdfocr = pypdfocr.pypdfocr:main'
-                ],
-        },
-    options = {
-	    "pyinstaller": {"packages": packages}
-	    },
-    cmdclass = {'test':PyTest}
-
-)
+    author_email="virantha@gmail.com",  # Removed.
+    package_data={'': ['*.xml']},
+    zip_safe=True,
+    include_package_data=True,
+    packages=packages,
+    install_requires=required,
+    entry_points={
+        'console_scripts': ['pypdfocr = pypdfocr.pypdfocr:main'],
+    },
+    options={"pyinstaller": {
+        "packages": packages
+    }},
+    cmdclass={
+        'test': PyTest
+    })
