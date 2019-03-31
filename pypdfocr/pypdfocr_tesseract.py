ec8f7557738ccd646f08c48145a7c3956e8ea1bf
mdbiz <martindudebiz@gmail.com>
2019-03-31 16:11:32 +0200

    PEP8

--- pypdfocr/pypdfocr_tesseract.py
+++ pypdfocr/pypdfocr_tesseract.py
@@ -13,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
 """
    Run Tesseract to generate hocr file 
 """
@@ -28,9 +26,11 @@ from subprocess import CalledProcessError
 from multiprocessing import Pool
 from pypdfocr_interrupts import init_worker
 
+
 def error(text):
     print("ERROR: %s" % text)
     sys.exit(-1)
+
 
 # Ugly hack to pass in object method to the multiprocessing library
 # From http://www.rueckstiess.net/research/snippets/show/ca1d7d90
@@ -38,22 +38,25 @@ def error(text):
 def unwrap_self(arg, **kwarg):
     return PyTesseract.make_hocr_from_pnm(*arg, **kwarg)
 
+
 class PyTesseract(object):
     """Class to wrap all the tesseract calls"""
+
     def __init__(self, config):
         """
            Detect windows tesseract location.  
         """
         self.lang = 'eng'
         self.required = "3.02.02"
-        self.threads = config.get('threads',4)
+        self.threads = config.get('threads', 4)
 
         if "binary" in config:  # Override location of binary
             binary = config['binary']
             if os.name == 'nt':
                 binary = '"%s"' % binary
                 binary = binary.replace("\\", "\\\\")
-            logging.info("Setting location for tesseracdt executable to %s" % (binary))
+            logging.info("Setting location for tesseracdt executable to %s" %
+                         (binary))
         else:
             if str(os.name) == 'nt':
                 # Explicit str here to get around some MagicMock stuff for testing that I don't quite understand
@@ -64,15 +67,18 @@ class PyTesseract(object):
         self.binary = binary
 
         self.msgs = {
-            'TS_MISSING': """ 
+            'TS_MISSING':
+            """ 
                 Could not execute %s
                 Please make sure you have Tesseract installed correctly
                 """ % self.binary,
-            'TS_VERSION':'Tesseract version is too old',
-            'TS_img_MISSING':'Cannot find specified tiff file',
-            'TS_FAILED': 'Tesseract-OCR execution failed!',
+            'TS_VERSION':
+            'Tesseract version is too old',
+            'TS_img_MISSING':
+            'Cannot find specified tiff file',
+            'TS_FAILED':
+            'Tesseract-OCR execution failed!',
         }
-
 
     def _is_version_uptodate(self):
         """
@@ -80,9 +86,10 @@ class PyTesseract(object):
         """
         logging.info("Checking tesseract version")
         cmd = '%s -v' % (self.binary)
-        logging.info(cmd)        
+        logging.info(cmd)
         try:
-            ret_output = subprocess.check_output(cmd, shell=True,  stderr=subprocess.STDOUT)
+            ret_output = subprocess.check_output(
+                cmd, shell=True, stderr=subprocess.STDOUT)
         except CalledProcessError:
             # Could not run tesseract
             error(self.msgs['TS_MISSING'])
@@ -91,49 +98,52 @@ class PyTesseract(object):
         for line in ret_output.splitlines():
             if 'tesseract' in line:
                 ver_str = line.split(' ')[1]
-                if ver_str.endswith('dev'): # Fix for version strings that end in 'dev'
+                if ver_str.endswith(
+                        'dev'):  # Fix for version strings that end in 'dev'
                     ver_str = ver_str[:-3]
-                if ver_str.startswith('v'): # Fix for version strings that add 'v' before version numbers
+                if ver_str.startswith(
+                        'v'
+                ):  # Fix for version strings that add 'v' before version numbers
                     ver_str = ver_str[1:]
 
         # Iterate through the version dots
         ver = [int(x) for x in ver_str.split('.')]
         req = [int(x) for x in self.required.split('.')]
 
-        # Aargh, in windows 3.02.02 is reported as version 3.02  
+        # Aargh, in windows 3.02.02 is reported as version 3.02
         # SFKM
         if str(os.name) == 'nt':
             req = req[:2]
 
         version_good = False
-        for i,num in enumerate(req):
-            if len(ver) < i+1:
+        for i, num in enumerate(req):
+            if len(ver) < i + 1:
                 # This minor version number is not present in tesseract, so it must be
                 # lower than required.  (3.02 < 3.02.01)
                 break
-            if ver[i]==num and len(ver) == i+1 and len(ver)==len(req):
+            if ver[i] == num and len(ver) == i + 1 and len(ver) == len(req):
                 # 3.02.02 == 3.02.02
                 version_good = True
                 continue
-            if ver[i]>num:
+            if ver[i] > num:
                 # 4.0 > 3.02.02
                 # 3.03.02 > 3.02.02
                 version_good = True
                 break
-            if ver[i]<num:
+            if ver[i] < num:
                 # 3.01.02 < 3.02.02
                 break
-            
+
         return version_good, ver_str
 
-    def _warn(self, msg): # pragma: no cover
+    def _warn(self, msg):  # pragma: no cover
         print("WARNING: %s" % msg)
 
-
     def make_hocr_from_pnms(self, fns):
-        uptodate,ver =  self._is_version_uptodate()
+        uptodate, ver = self._is_version_uptodate()
         if not uptodate:
-            error(self.msgs['TS_VERSION']+ " (found %s, required %s)" % (ver, self.required))
+            error(self.msgs['TS_VERSION'] + " (found %s, required %s)" %
+                  (ver, self.required))
 
         # Glob it
         #fns = glob.glob(img_filename)
@@ -141,7 +151,7 @@ class PyTesseract(object):
         pool = Pool(processes=self.threads, initializer=init_worker)
 
         try:
-            hocr_filenames = pool.map(unwrap_self, zip([self]*len(fns), fns))
+            hocr_filenames = pool.map(unwrap_self, zip([self] * len(fns), fns))
             pool.close()
         except KeyboardInterrupt or Exception:
             print("Caught keyboard interrupt... terminating")
@@ -150,27 +160,29 @@ class PyTesseract(object):
         finally:
             pool.join()
 
-        return zip(fns,hocr_filenames)
-
+        return zip(fns, hocr_filenames)
 
     def make_hocr_from_pnm(self, img_filename):
 
-        basename,filext = os.path.splitext(img_filename)
+        basename, filext = os.path.splitext(img_filename)
         hocr_filename = "%s.html" % basename
 
         if not os.path.exists(img_filename):
             error(self.msgs['TS_img_MISSING'] + " %s" % (img_filename))
 
-        logging.info("Running OCR on %s to create %s.html" % (img_filename, basename))
-        cmd = '%s "%s" "%s" --psm 1 -c hocr_font_info=1 -l %s hocr' % (self.binary, img_filename, basename, self.lang)
+        logging.info("Running OCR on %s to create %s.html" % (img_filename,
+                                                              basename))
+        cmd = '%s "%s" "%s" --psm 1 -c hocr_font_info=1 -l %s hocr' % (
+            self.binary, img_filename, basename, self.lang)
         logging.info(cmd)
         try:
-            ret_output = subprocess.check_output(cmd, shell=True,  stderr=subprocess.STDOUT)
+            ret_output = subprocess.check_output(
+                cmd, shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             # Could not run tesseract
             print e.output
-            self._warn (self.msgs['TS_FAILED'])
-                
+            self._warn(self.msgs['TS_FAILED'])
+
         if os.path.isfile(hocr_filename):
             # Output format is html for old versions of tesseract
             logging.info("Created %s.html" % basename)
@@ -183,4 +195,3 @@ class PyTesseract(object):
                 return hocr_filename
             else:
                 error(self.msgs['TS_FAILED'])
-            
