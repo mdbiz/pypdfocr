504e8da66b2da8b4569a9b378e09df6b6bd7c87d
mdbiz <martindudebiz@gmail.com>
2019-03-31 15:59:51 +0200

    PEP8

--- test/test_tesseract.py
+++ test/test_tesseract.py
@@ -7,15 +7,15 @@ import hashlib
 
 from mock import patch, call
 
-class TestTesseract:
 
-    @pytest.mark.skipif(os.name=='nt', reason='Does not work on Windows')
+class TestTesseract:
+    @pytest.mark.skipif(os.name == 'nt', reason='Does not work on Windows')
     def test_version_shorter_older(self):
         with patch("subprocess.check_output") as mock_subprocess:
             p = P.PyTesseract({})
             p.required = "3.02.02"
             mock_subprocess.return_value = """tesseract 3.02"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (not uptodate)
 
     def test_version_minor_older(self):
@@ -23,7 +23,7 @@ class TestTesseract:
             p = P.PyTesseract({})
             p.required = "3.02.02"
             mock_subprocess.return_value = """tesseract 3.02.01"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (not uptodate)
 
     def test_version_major_older(self):
@@ -31,16 +31,16 @@ class TestTesseract:
             p = P.PyTesseract({})
             p.required = "3.02.02"
             mock_subprocess.return_value = """tesseract 2.03.03"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (not uptodate)
 
-    @pytest.mark.skipif(os.name=='nt', reason='Does not work on Windows')
+    @pytest.mark.skipif(os.name == 'nt', reason='Does not work on Windows')
     def test_version_major_equal(self):
         with patch("subprocess.check_output") as mock_subprocess:
             p = P.PyTesseract({})
             p.required = "3.02.02"
             mock_subprocess.return_value = """tesseract 3.02.02"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (uptodate)
 
     def test_version_major_newer(self):
@@ -49,7 +49,7 @@ class TestTesseract:
             p.required = "3.02.02"
 
             mock_subprocess.return_value = """tesseract 4.01"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (uptodate)
 
     def test_version_minor_newer(self):
@@ -58,13 +58,12 @@ class TestTesseract:
             p.required = "3.01.02"
 
             mock_subprocess.return_value = """tesseract 3.02"""
-            uptodate,ver = p._is_version_uptodate()
+            uptodate, ver = p._is_version_uptodate()
             assert (uptodate)
-
 
     def test_tesseract_presence(self, capsys):
         p = P.PyTesseract({})
-        p.binary = "tesserac" # Misspell it and make sure we get an error
+        p.binary = "tesserac"  # Misspell it and make sure we get an error
         with pytest.raises(SystemExit):
             p._is_version_uptodate()
         out, err = capsys.readouterr()
@@ -96,19 +95,20 @@ class TestTesseract:
         p.required = "3.02.02"
 
         mock_subprocess.return_value = """tesseract 3.02"""
-        uptodate,ver = p._is_version_uptodate()
+        uptodate, ver = p._is_version_uptodate()
         assert (uptodate)
 
     @patch('pypdfocr.pypdfocr_tesseract.PyTesseract._is_version_uptodate')
     @patch('pypdfocr.pypdfocr_tesseract.os.name')
     @patch('pypdfocr.pypdfocr_tesseract.os.path.exists')
-    def test_force_Nt(self, mock_os_path_exists, mock_os_name, mock_uptodate, capsys):
+    def test_force_Nt(self, mock_os_path_exists, mock_os_name, mock_uptodate,
+                      capsys):
         mock_os_name.__str__.return_value = 'nt'
         p = P.PyTesseract({})
         assert ('tesseract.exe' in p.binary)
 
-        mock_os_path_exists.return_value = True 
-        mock_uptodate.return_value = (True,"")
+        mock_os_path_exists.return_value = True
+        mock_uptodate.return_value = (True, "")
         # force a bad tesseract on windows
         p.binary = "blah"
         print("here")
@@ -119,7 +119,8 @@ class TestTesseract:
     @patch('pypdfocr.pypdfocr_tesseract.PyTesseract._is_version_uptodate')
     @patch('pypdfocr.pypdfocr_tesseract.os.name')
     @patch('pypdfocr.pypdfocr_tesseract.os.path.exists')
-    def test_tesseract_fail(self, mock_os_path_exists, mock_os_name, mock_uptodate, mock_subprocess_call,capsys):
+    def test_tesseract_fail(self, mock_os_path_exists, mock_os_name,
+                            mock_uptodate, mock_subprocess_call, capsys):
         """
             Get all the checks past and make sure we report the case where tesseract returns a non-zero status
         """
@@ -127,12 +128,11 @@ class TestTesseract:
         p = P.PyTesseract({})
         assert ('tesseract.exe' in p.binary)
 
-        mock_os_path_exists.return_value = True 
-        mock_uptodate.return_value = (True,"")
+        mock_os_path_exists.return_value = True
+        mock_uptodate.return_value = (True, "")
         mock_subprocess_call.return_value = -1
         with pytest.raises(SystemExit):
             p.make_hocr_from_pnm('blah.tiff')
 
         out, err = capsys.readouterr()
         assert p.msgs['TS_FAILED'] in out
-
